69db71ad202b9dda9e2d147a7840144b1f9e3336
frederike.ramin <frederike.ramin@student.hpi.de>
2019-06-20 11:39:45 +0200

    Add input variable map

--- lib/utils.dart
+++ lib/utils.dart
@@ -26,28 +26,48 @@ class YesNoWrapper {
 
 class Inputs {
   DoubleWrapper age = new DoubleWrapper(18.0);
-  DoubleWrapper height = new DoubleWrapper(120.0);
-  DoubleWrapper weight = new DoubleWrapper(30);
-  DoubleWrapper diastolicBloodPressure = new DoubleWrapper(30.0);
-  DoubleWrapper systolicBloodPressure = new DoubleWrapper(70.0);
-  DoubleWrapper noOfCigarettesPerDay = new DoubleWrapper(0.0);
-  DoubleWrapper noOfCigarettesPreviouslyPerDay = new DoubleWrapper(0.0);
-  Sex sex;
   AlcoholFrequency alcoholFrequency;
-  YesNoWrapper currentlySmoking = new YesNoWrapper(null);
-  YesNoWrapper neverSmoked = new YesNoWrapper(null);
-  YesNoWrapper coughOnMostDays = new YesNoWrapper(null);
   YesNoWrapper asthma = new YesNoWrapper(null);
   YesNoWrapper copd = new YesNoWrapper(null);
+  YesNoWrapper coughOnMostDays = new YesNoWrapper(null);
+  YesNoWrapper currentlySmoking = new YesNoWrapper(null);
   YesNoWrapper diabetes = new YesNoWrapper(null);
+  DoubleWrapper diastolicBloodPressure = new DoubleWrapper(30.0);
+  DoubleWrapper height = new DoubleWrapper(120.0);
+  YesNoWrapper neverSmoked = new YesNoWrapper(null);
+  DoubleWrapper noOfCigarettesPerDay = new DoubleWrapper(0.0);
+  DoubleWrapper noOfCigarettesPreviouslyPerDay = new DoubleWrapper(0.0);
   YesNoWrapper previouslySmoked = new YesNoWrapper(null);
+  Sex sex;
   YesNoWrapper sputumOnMostDays = new YesNoWrapper(null);
-  YesNoWrapper wheezeInChestInLastYear = new YesNoWrapper(null);
+  DoubleWrapper systolicBloodPressure = new DoubleWrapper(70.0);
   YesNoWrapper tuberculosis = new YesNoWrapper(null);
+  DoubleWrapper weight = new DoubleWrapper(30);
+  YesNoWrapper wheezeInChestInLastYear = new YesNoWrapper(null);
 
   getVariable(String variableName) {
     Map<String, dynamic> map = {
+      'age': this.age,
+      'alcoholFrequency': this.alcoholFrequency,
+      'asthma': this.asthma,
       'COPD': this.copd,
+      'coughOnMostDays': this.coughOnMostDays,
+      'currentlySmoking': this.currentlySmoking,
+      'diabetes': this.diabetes,
+      'diastolicBloodPRessure': this.diastolicBloodPressure,
+      'height': this.height,
+      'neverSmoked': this.neverSmoked,
+      'noOfCigarettesPerDay': this.noOfCigarettesPerDay,
+      'noOfCigarettesPreviouslyPerDay': this.noOfCigarettesPreviouslyPerDay,
+      'previouslySmoked': this.previouslySmoked,
+      'sex': this.sex,
+      'sputumOnMostDays': this.sputumOnMostDays,
+      'systolicBloodPressure': this.systolicBloodPressure,
+      'tuberculosis': this.tuberculosis,
+      'weight': this.weight,
+      'wheezeInChestInLastYear': this.wheezeInChestInLastYear,
     };
+
+    return map[variableName];
   }
 }
