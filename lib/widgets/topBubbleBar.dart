14b44df45ee30d0344a20c2155bb6f0894183574
frederike.ramin <frederike.ramin@student.hpi.de>
2019-07-01 11:28:02 +0200

    Add disease bubbles

--- lib/widgets/topBubbleBar.dart
+++ lib/widgets/topBubbleBar.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:bars_frontend/main.dart';
 import 'dialogs.dart';
 
-Widget getPatientImage(double globalWidth, double globalHeight) {
-  double width = 200;
+Widget getPatientImage(double width, Offset position) {
   return Positioned(
     child: Image(image: AssetImage('assets/man-user.png'), width: width),
-    left: globalWidth / 2 - width / 2,
-    top: globalHeight / 4,
+    left: position.dx,
+    top: position.dy,
   );
 }
 
 Widget getTopBubbleBar(
     MyHomePageState homePageState, double globalWidth, double globalHeight) {
+  double imageDimensions = 200;
+  double xOffset = globalWidth / 2 - imageDimensions / 2;
+  double yOffset = globalHeight / 4;
+  Offset imagePosition = Offset(xOffset, yOffset);
+
   return Expanded(
     child: Row(
       children: <Widget>[
@@ -27,7 +31,11 @@ Widget getTopBubbleBar(
                   asyncCOPDInputDialog),
               DragBubble(Offset(300, 0.0), homePageState, "Never Smoked",
                   asyncNeverSmokedInputDialog),
-              getPatientImage(globalWidth, globalHeight),
+              getPatientImage(imageDimensions, imagePosition),
+              DiseaseBubble("COPD", Offset(imagePosition.dx - 90, imagePosition.dy)),
+              DiseaseBubble("Asthma", Offset(imagePosition.dx + imageDimensions, imagePosition.dy)),
+              DiseaseBubble("Tuberculosis", Offset(imagePosition.dx - 90, imagePosition.dy + imageDimensions)),
+              DiseaseBubble("Diabetes", Offset(imagePosition.dx + imageDimensions, imagePosition.dy + imageDimensions)),
             ],
           ),
         ),
@@ -120,5 +128,41 @@ class Bubble extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class DiseaseBubble extends StatelessWidget {
+  final String title;
+  final Offset position;
+
+  DiseaseBubble(this.title, this.position);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        top: position.dy,
+        left: position.dx,
+        child: Container(
+          width: 90,
+          height: 90,
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.orange,
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                  )),
+            ],
+          ),
+        ));
   }
 }
