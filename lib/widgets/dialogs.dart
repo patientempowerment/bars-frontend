e7379fa80fc25a0dbb5c2ed807a6f8261a178046
davidimmhahn <david.hahn@student.hpi.de>
2019-07-24 13:57:22 +0200

    change enter information dialog to match visuals of app

--- lib/widgets/dialogs.dart
+++ lib/widgets/dialogs.dart
@@ -2,32 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:bars_frontend/main.dart';
 import 'package:bars_frontend/utils.dart';
 
-Future<dynamic> asyncInputDialog(
-    BuildContext context,
-    MyHomePageState homePageState,
-    MapEntry<String, dynamic> feature) async {
+Future<dynamic> asyncInputDialog(BuildContext context,
+    MyHomePageState homePageState, MapEntry<String, dynamic> feature) async {
   return showDialog<dynamic>(
     context: context,
     barrierDismissible: true,
     builder: (BuildContext context) {
       return Container(
         child: SimpleDialog(
+          titlePadding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+          title: Text(
+            "Please enter your information",
+            textAlign: TextAlign.center,
+          ),
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Column(
-                children: <Widget>[
-                  MyDialogContent(homePageState, feature),
-                  Container(height: 50),
-                  SimpleDialogOption(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 20),
+              child: Column(children: <Widget>[
+                MyDialogContent(homePageState, feature),
+                Container(
+                  height: 40,
+                  width: 100,
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(8.0),
                     onPressed: () {
                       Navigator.of(context)
                           .pop(homePageState.userInputs[feature.key]);
                     },
-                    child: new Text('Ok'),
+                    child: Text("OK"),
                   ),
-                ],
-              ),
+                ),
+              ]),
             )
           ],
         ),
@@ -57,13 +64,11 @@ class MyDialogContentState extends State<MyDialogContent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 200,
-        width: 500,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              buildInputWidget(
-                  homePageState, this, feature, homePageState.userInputs)
-            ]));
+        height: 150,
+        width: 600,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          buildInputWidget(
+              homePageState, this, feature, homePageState.userInputs)
+        ]));
   }
 }
