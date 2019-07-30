520885cc84f5b948ece0883c38f95690ab01a645
Kim Borchart <kim-pascal.borchart@student.hpi.de>
2019-07-30 13:58:46 +0200

    format the long long man

--- lib/widgets/adminSettings.dart
+++ lib/widgets/adminSettings.dart
@@ -10,24 +10,31 @@ class AdminSettings extends StatefulWidget {
   final MyHomePageState homePageState;
 
   AdminSettings(this.homePageState);
+
   @override
   State<StatefulWidget> createState() {
-    return AdminSettingsState(homePageState, homePageState.serverConfig, homePageState.featureConfig);
+    return AdminSettingsState(
+        homePageState, homePageState.serverConfig, homePageState.featureConfig);
   }
 }
 
 class AdminSettingsState extends State<AdminSettings> {
   MyHomePageState homePageState;
+
   AdminSettingsState(this.homePageState, this.serverConfig, this.features);
+
   final serverConfig;
   Map<String, dynamic> features;
   static Key formKey = new UniqueKey();
+
   //List<FeatureTileContent> featureTiles;
 
   _setFeatureTile(title) {
     for (var f in features.entries) {
       if (f.key == title) {
-        (f.value["selected"]??= false) ? f.value["selected"] = false : f.value["selected"] = true;
+        (f.value["selected"] ??= false)
+            ? f.value["selected"] = false
+            : f.value["selected"] = true;
       }
     }
   }
@@ -37,59 +44,49 @@ class AdminSettingsState extends State<AdminSettings> {
     return SizedBox(
         width: homePageState.globalWidth,
         child: Drawer(
-            child: Column(
-                children: <Widget>[
-                    Row(
-                        children: <Widget>[
-                          Flexible(
-                            child:TextField(
-                                key: formKey,
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    hintText: 'Enter collection to use'
-                                ),
-                                onSubmitted: (String newCollection) {
-                                  getFeatureConfig(
-                                      homePageState.serverConfig['address'],
-                                      jsonEncode(homePageState.serverConfig['database']),
-                                      homePageState.serverConfig['fallbacks']
-                                      ['feature-config'])
-                                  .then((result) {
-                                    setState(() {
-                                      features = result;
-                                      for (var feature in features.entries){
-                                        feature.value["selected"] = false;
-                                      }
-                                    });
-                                  });
-                                }
-                            )
-                          )
-                        ]
-                    ),
-                  Flexible(
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: <Widget> [
-                        for (var feature in features.entries) ListTile(
-                            key: Key(feature.key),
-                            title: Text('${feature.key}'),
-                            onTap: () => setState(() {
-                              _setFeatureTile(feature.key);
-                            }),
-                            selected: feature.value["selected"] ?? false
-                        )
-                      ]
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      NextStepButton(this)
-                    ],
-                  )
+            child: Column(children: <Widget>[
+          Row(children: <Widget>[
+            Flexible(
+                child: TextField(
+                    key: formKey,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter collection to use'),
+                    onSubmitted: (String newCollection) {
+                      getFeatureConfig(
+                              homePageState.serverConfig['address'],
+                              jsonEncode(
+                                  homePageState.serverConfig['database']),
+                              homePageState.serverConfig['fallbacks']
+                                  ['feature-config'])
+                          .then((result) {
+                        setState(() {
+                          features = result;
+                          for (var feature in features.entries) {
+                            feature.value["selected"] = false;
+                          }
+                        });
+                      });
+                    }))
+          ]),
+          Flexible(
+            child: ListView(shrinkWrap: true, children: <Widget>[
+              for (var feature in features.entries)
+                ListTile(
+                    key: Key(feature.key),
+                    title: Text('${feature.key}'),
+                    onTap: () => setState(() {
+                          _setFeatureTile(feature.key);
+                        }),
+                    selected: feature.value["selected"] ?? false)
+            ]),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[NextStepButton(this)],
+          )
         ]))
         /*child: ReorderableListView(
               padding: EdgeInsets.zero,
@@ -128,11 +125,11 @@ class NextStepButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      child: Icon(Icons.arrow_forward_ios),
-      onPressed: () {
-        adminSettingsState.setState((){
-          var x = 1;
+        child: Icon(Icons.arrow_forward_ios),
+        onPressed: () {
+          adminSettingsState.setState(() {
+            var x = 1;
+          });
         });
-      });
   }
 }
